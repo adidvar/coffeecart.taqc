02639a7297c21e5ca6117320ba15d8b36d4f3f78
Liubomyr Halamaha <galamagal86@gmail.com>
2025-04-25 13:28:31 +0300

    added base component

---
+++ src/main/java/com/coffeecart/ui/component/CardComponent.java
@@ -0,0 +1,10 @@
+package com.coffeecart.ui.component;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CardComponent extends BaseComponent {
+    public CardComponent(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+}
---
+++ src/main/java/com/coffeecart/ui/page/MenuPO.java
@@ -0,0 +1,25 @@
+package com.coffeecart.ui.page;
+
+import com.coffeecart.ui.component.CardComponent;
+import lombok.Getter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class MenuPO extends BasePage {
+    @Getter
+    private List<CardComponent> cards;
+
+    @FindBy(xpath="//*[@id='app']/div[2]/ul/li")
+    private List<WebElement> rootCards;
+
+    public MenuPO(WebDriver driver) {
+        super(driver);
+
+        for(WebElement card: rootCards) {
+            cards.add(new CardComponent(driver, card));
+        }
+    }
+}
