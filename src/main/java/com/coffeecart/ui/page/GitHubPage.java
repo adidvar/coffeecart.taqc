5664f129363672dc1601e1c43b1408408363c189
Dmytro Zubenko <fokist@outlook.com>
2025-04-26 01:07:24 +0300

    git hub page corrected

--- src/main/java/com/coffeecart/ui/page/GitHubPage.java
+++ src/main/java/com/coffeecart/ui/page/GitHubPage.java
@@ -1,13 +1,13 @@
 package com.coffeecart.ui.page;
 
-import com.coffeecart.ui.Base;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
 
-public class GitHubPage extends Base {
+public class GitHubPage extends BasePage {
     @FindBy(xpath = "//*[@class='container']//a[@href]")
     private List<WebElement> pageLinks;
 
@@ -15,6 +15,7 @@ public class GitHubPage extends Base {
         super(driver);
     }
 
+    @Step("Get page links")
     public List<String> getPageLinks() {
         waitUntilAllElementsVisible(pageLinks);
         return pageLinks.stream()
