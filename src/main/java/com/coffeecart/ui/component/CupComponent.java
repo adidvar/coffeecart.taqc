9c9ed1de2f3f0e578b0b6c5b9f161925da2520d2
Наталія <138515375+HrushaNataliia@users.noreply.github.com>
2025-04-25 19:49:00 +0300

    added CupComponent

--- src/main/java/com/coffeecart/ui/component/CupComponent.java
+++ src/main/java/com/coffeecart/ui/component/CupComponent.java
@@ -1,7 +1,11 @@
 package com.coffeecart.ui.component;
 
-public class CupComponent{
-    public String pass(){
-        return "pass";
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CupComponent extends BaseComponent{
+    public CupComponent(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
     }
+
 }
