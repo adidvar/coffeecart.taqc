6bc7808794bdfc4a12c418017f9a7874679097be
Dmytro Zubenko <fokist@outlook.com>
2025-04-25 17:24:42 +0300

    added a GitHub page

---
+++ src/main/java/com/coffeecart/ui/page/GitHubPage.java
@@ -0,0 +1,10 @@
+package com.coffeecart.ui.page;
+
+import com.coffeecart.ui.Base;
+import org.openqa.selenium.WebDriver;
+
+public class GitHubPage extends Base {
+    public GitHubPage(WebDriver driver) {
+        super(driver);
+    }
+}
