3653d7c7930ee60bcfecd0e617528fee1c1d2d6b
viktoriiafylyk <vfilik194@gmail.com>
2025-04-25 22:33:45 +0300

    Header Component added

--- src/main/java/com/coffeecart/ui/component/HeaderComponent.java
+++ src/main/java/com/coffeecart/ui/component/HeaderComponent.java
@@ -1,10 +1,31 @@
 package com.coffeecart.ui.component;
 
+import lombok.Getter;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-public class HeaderComponent extends BaseComponent{
+@Getter
+public class HeaderComponent extends BaseComponent {
+    @FindBy(xpath = "//a[@aria-label='Menu page']")
+    private WebElement menuLink;
+
+    @FindBy(xpath = "//a[@aria-label='Cart page']")
+    private WebElement cartLink;
+
+    @FindBy(xpath = "//a[@aria-label='GitHub page']")
+    private WebElement githubLink;
+
     public HeaderComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
     }
+
+    @Step("Click on Cart page")
+    public CartPage navigateToCart() {
+        waitUntilElementClickable(cartLink);
+        cartLink.click();
+        return new CartPage(driver);
+    }
+
 }
