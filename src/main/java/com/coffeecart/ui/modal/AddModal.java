4ea2e13fce5cff2b791012b9cb36ab1673866477
BohdanKovalets <kovaletss.bogdan@gmail.com>
2025-04-25 20:00:20 +0300

    added classes AddModal and SuccessfulPopUp

---
+++ src/main/java/com/coffeecart/ui/modal/AddModal.java
@@ -0,0 +1,10 @@
+package com.coffeecart.ui.modal;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class AddModal extends BaseModal{
+    public AddModal(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+}
