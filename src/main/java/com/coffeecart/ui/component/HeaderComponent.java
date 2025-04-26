ec981a26f9a325ad992be22032eaf97429401179
viktoriiafylyk <vfilik194@gmail.com>
2025-04-26 12:20:08 +0300

    updated header component

--- src/main/java/com/coffeecart/ui/component/HeaderComponent.java
+++ src/main/java/com/coffeecart/ui/component/HeaderComponent.java
@@ -2,26 +2,36 @@ package com.coffeecart.ui.component;
 
 import com.coffeecart.ui.page.CartPage;
 import com.coffeecart.ui.page.GitHubPage;
-
+import com.coffeecart.ui.page.MenuPage;
 import lombok.Getter;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-@Getter
+
 public class HeaderComponent extends BaseComponent {
+    @Getter
     @FindBy(xpath = "//a[@aria-label='Menu page']")
     private WebElement menuLink;
 
+    @Getter
     @FindBy(xpath = "//a[@aria-label='Cart page']")
     private WebElement cartLink;
 
+    @Getter
     @FindBy(xpath = "//a[@aria-label='GitHub page']")
     private WebElement githubLink;
 
     public HeaderComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
+    }
+
+    @Step("Click on Menu page")
+    public MenuPage navigateToMenu() {
+        waitUntilElementClickable(menuLink);
+        menuLink.click();
+        return new MenuPage(driver);
     }
 
     @Step("Click on Cart page")
