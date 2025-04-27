df3171c19438f21fb0d8b97cee78b866f9ff61cb
adidvar <adidvar@gmail.com>
2025-04-27 12:53:30 +0300

    refactor

--- src/main/java/com/coffeecart/ui/elements/TotalButtonElement.java
+++ src/main/java/com/coffeecart/ui/elements/TotalButtonElement.java
@@ -2,6 +2,7 @@ package com.coffeecart.ui.elements;
 
 import com.coffeecart.ui.component.CartComponent;
 import com.coffeecart.ui.modal.PaymentDetail;
+import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -9,12 +10,15 @@ import org.openqa.selenium.support.FindBy;
 
 public class TotalButtonElement extends BaseElement {
 
+    @Getter
     @FindBy(xpath = ".//button[@class='pay']")
     WebElement totalButton;
 
+    @Getter
     @FindBy(xpath = ".//ul[@class='cart-preview']")
     WebElement cardComponentElement;
 
+    @Getter
     @FindBy(xpath = "//div[@class='modal']")
     WebElement modalElement;
 
@@ -28,7 +32,8 @@ public class TotalButtonElement extends BaseElement {
         actions = new Actions(driver);
     }
 
-    public PaymentDetail click(){
+    public PaymentDetail clickTotalButton(){
+        waitUntilElementClickable(totalButton);
         totalButton.click();
         return new PaymentDetail(driver,modalElement);
     }
@@ -37,8 +42,9 @@ public class TotalButtonElement extends BaseElement {
         return Double.parseDouble(totalButton.getText().replace("Total: $",""));
     }
 
-    public CartComponent hover(){
+    public CartComponent hoverTotalButton(){
         actions.moveToElement(totalButton).perform();
+        waitUntilElementVisible(cardComponentElement);
         return cartComponent;
     }
 }
