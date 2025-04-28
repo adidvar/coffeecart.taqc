dace6fd1ec5de227ca3e2e64406c58741b50c544
Roman <romankmet10@gmail.com>
2025-04-28 22:00:32 +0300

    CardComponent.java added methods

--- src/main/java/com/coffeecart/ui/component/CardComponent.java
+++ src/main/java/com/coffeecart/ui/component/CardComponent.java
@@ -3,6 +3,7 @@ package com.coffeecart.ui.component;
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 
@@ -16,6 +17,9 @@ public class CardComponent extends BaseComponent {
     @Getter
     @FindBy(xpath = ".//div[contains(@class, 'cup')]")
     private WebElement cupRootElement;
+    @Getter
+    @FindBy(xpath = ".//p[contains(@class, 'description')]")
+    private WebElement descriptionElement;
 
     private CupComponent cupComponent;
 
@@ -31,6 +35,41 @@ public class CardComponent extends BaseComponent {
     public double getPrice() {
         String priceText = priceElement.getText().replace("$", "").trim();
         return Double.parseDouble(priceText);
+    }
+    public String getCupBorderColorOnHover() {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(cupRootElement).perform();
+        return cupRootElement.getCssValue("border-color");
+    }
+
+    public String getNameColorOnHover() {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(nameElement).perform();
+        return nameElement.getCssValue("color");
+    }
+
+    public String getDescription() {
+        return descriptionElement.getText().trim();
+    }
+
+    public void clickOnCup() {
+        cupRootElement.click();
+    }
+
+    public void clickOnName() {
+        nameElement.click();
+    }
+
+    public String getNameFontSize() {
+        return nameElement.getCssValue("font-size");
+    }
+
+    public String getCupBackgroundColor() {
+        return cupRootElement.getCssValue("background-color");
+    }
+
+    public boolean isCupDisplayed() {
+        return cupRootElement.isDisplayed();
     }
 
     public CupComponent getCupComponent() {
