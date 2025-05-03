a1c69a9eb1dd7e0f1c57c0df983110875229889e
Dmytro Zubenko <fokist@outlook.com>
2025-05-03 22:25:47 +0300

    test improvements

--- src/test/java/com/coffeecart/ui/discardLuckyProposition/DiscardLuckyPropositionTest.java
+++ src/test/java/com/coffeecart/ui/discardLuckyProposition/DiscardLuckyPropositionTest.java
@@ -1,13 +1,10 @@
 package com.coffeecart.ui.discardLuckyProposition;
 
 import com.coffeecart.ui.component.ShortItemComponent;
-import com.coffeecart.ui.elements.TotalButtonElement;
 import com.coffeecart.ui.page.MenuPage;
 import com.coffeecart.ui.testrunners.BaseTestRunner;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -15,18 +12,10 @@ import java.util.Optional;
 
 @Slf4j
 public class DiscardLuckyPropositionTest extends BaseTestRunner {
-    MenuPage menuPage;
-    TotalButtonElement totalButtonElement;
-
-    @BeforeClass
-    public void setUp() {
-        super.beforeClass();
-        menuPage = new MenuPage(driver);
-        totalButtonElement = new TotalButtonElement(driver, menuPage.getPaymentModalRoot());
-    }
 
     @Test
     public void testLuckyPropositionDiscard() {
+        MenuPage menuPage = new MenuPage(driver);
         List<ShortItemComponent> items = menuPage
                 .clickDrink("Flat White")
                 .getButtonElement()
