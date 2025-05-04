4f6aede21fd10d8a9a7846da0756d8520499ab35
MarryMik <57491436+MarryMik@users.noreply.github.com>
2025-05-04 20:54:27 +0300

    minor changes

--- src/test/java/com/coffeecart/ui/TestPossibilityAddingMocha.java
+++ src/test/java/com/coffeecart/ui/TestPossibilityAddingMocha.java
@@ -4,14 +4,14 @@ import com.coffeecart.data.DrinkEnum;
 import com.coffeecart.data.MenuPageDataProviders;
 import com.coffeecart.ui.component.LuckyDayComponent;
 import com.coffeecart.ui.page.MenuPage;
-import com.coffeecart.ui.testrunners.BaseTestRunner;
 import io.qameta.allure.*;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 
-public class TestPossibilityAddingMocha extends BaseTestRunner {
+public class TestPossibilityAddingMocha extends BaseTest {
+    private final String expectedTitleLuckyDayComponent = "It's your lucky day! Get an extra cup of Mocha for $4.";
 
     @Test(dataProvider = "drinkNames", dataProviderClass = MenuPageDataProviders.class)
     @Description("Verification of the possibility of adding Mocha to the cart in the pop-up 'Lucky Day'")
@@ -38,7 +38,6 @@ public class TestPossibilityAddingMocha extends BaseTestRunner {
         softAssert.assertTrue(menuPage.getLuckyDayModalRoot().isDisplayed(),
                 "The LuckyDayModal should be displayed.");
 
-        String expectedTitleLuckyDayComponent = "It's your lucky day! Get an extra cup of Mocha for $4.";
         softAssert.assertEquals(luckyDayComponent.getTitle().getText(), expectedTitleLuckyDayComponent,
                 String.format("The title of LuckyDayComponent should be '%s' but was '%s'",
                         expectedTitleLuckyDayComponent,
