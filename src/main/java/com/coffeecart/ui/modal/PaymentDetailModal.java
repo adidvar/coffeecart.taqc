524d801a28be5562f85b1cc4e118a3e7f4739291
Наталія <138515375+HrushaNataliia@users.noreply.github.com>
2025-05-04 12:21:36 +0300

    added PaymentSubmissionWithValidDataTest

--- src/main/java/com/coffeecart/ui/modal/PaymentDetailModal.java
+++ src/main/java/com/coffeecart/ui/modal/PaymentDetailModal.java
@@ -10,17 +10,32 @@ import org.openqa.selenium.support.FindBy;
 
 public class PaymentDetailModal extends BaseModal {
     @Getter
+    @FindBy(xpath = ".//h1")
+    private WebElement header;
+    @Getter
+    @FindBy(xpath = ".//p")
+    private WebElement paragraph;
+    @Getter
+    @FindBy(xpath = ".//label[@for='name']")
+    private WebElement labelName;
+    @Getter
     @FindBy(xpath = ".//input[@id='name']")
-    private WebElement name;
+    private WebElement inputName;
+    @Getter
+    @FindBy(xpath = ".//label[@for='email']")
+    private WebElement labelEmail;
     @Getter
     @FindBy(xpath = ".//input[@id='email']")
-    private WebElement email;
+    private WebElement inputEmail;
+    @Getter
+    @FindBy(xpath = ".//label[@id='promotion-label']")
+    private WebElement labelCheckbox;
+    @Getter
+    @FindBy(xpath = ".//input[@id='promotion']")
+    private WebElement inputCheckbox;
     @Getter
     @FindBy(xpath = ".//button[@id='submit-payment']")
     private WebElement submitButton;
-    @Getter
-    @FindBy(xpath = ".//input[@id='promotion']")
-    private WebElement subscriptionCheckbox;
     @Getter
     @FindBy(xpath = ".//section/button")
     private WebElement closeModalWindowButton;
@@ -29,31 +44,63 @@ public class PaymentDetailModal extends BaseModal {
         super(driver, rootElement);
     }
 
+    public String getHeaderText() {
+        return header.getText();
+    }
+
+    public String getParagraphText() {
+        return paragraph.getText();
+    }
+
+    public String getLabelNameText() {
+        return labelName.getText();
+    }
+
+    public String getLabelEmailText() {
+        return labelEmail.getText();
+    }
+
+    public String getLabelCheckboxText() {
+        return labelCheckbox.getText();
+    }
+
+    public String getSubmitButtonText() {
+        return submitButton.getText();
+    }
+
     @Step("Set Name Input {name}")
     public PaymentDetailModal enterName(String name) {
-        waitUntilElementVisible(getName());
-        getName().sendKeys(name);
+        waitUntilElementVisible(inputName);
+        inputName.sendKeys(name);
         return this;
     }
 
     @Step("Set Email Input {email}")
     public PaymentDetailModal enterEmail(String email) {
-        waitUntilElementVisible(getEmail());
-        getEmail().sendKeys(email);
+        waitUntilElementVisible(inputEmail);
+        inputEmail.sendKeys(email);
         return this;
+    }
+
+    public boolean isCheckboxMarked() {
+        String gtmValue = inputCheckbox.getAttribute("data-gtm-form-interact-field-id");
+        if ("2".equals(gtmValue)) {
+            return true;
+        }
+        return false;
     }
 
     @Step("Mark the check box")
     public PaymentDetailModal markCheckbox() {
-        waitUntilElementVisible(getSubscriptionCheckbox());
-        getSubscriptionCheckbox().click();
+        waitUntilElementVisible(inputCheckbox);
+        inputCheckbox.click();
         return this;
     }
 
     @Step("Submit button click with valid values on the Menu page")
-    public MenuPage clickSubmitButtonWithValidInput() {
+    public SuccessfulPopUp clickSubmitButtonWithValidInput() {
         getSubmitButton().click();
-        return new MenuPage(driver);
+        return new SuccessfulPopUp(driver,rootElement);
     }
 
     @Step("Submit button click with invalid values")
@@ -62,10 +109,6 @@ public class PaymentDetailModal extends BaseModal {
         return this;
     }
 
-    @Step("Submit button get text")
-    public String getSubmitButtonText() {
-        return getSubmitButton().getText();
-    }
 
     @Step("Close modal window")
     public MenuPage closeModalWindowOnMenuPage() {
---
+++ src/test/java/com/coffeecart/ui/PaymentSubmissionWithValidDataTest.java
@@ -0,0 +1,60 @@
+package com.coffeecart.ui;
+
+
+import com.coffeecart.ui.modal.PaymentDetailModal;
+import com.coffeecart.ui.modal.SuccessfulPopUp;
+import com.coffeecart.ui.page.MenuPage;
+import com.coffeecart.ui.testrunners.BaseTestRunner;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Owner;
+import jdk.jfr.Description;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+
+public class PaymentSubmissionWithValidDataTest extends BaseTestRunner {
+
+    private static final String SUCCESS_MESSAGE = "Thanks for your purchase. Please check your email for payment.";
+    private static final String ESPRESSO_DRINK = "Espresso";
+
+    @Test
+    @Description("Verify valid name/email submission shows confirmation message.")
+    @Feature("Payment Form Validation")
+    @Issue("3")
+    @Owner("Nataliia Hrusha")
+    public void verifyPaymentSubmissionWithValidData() {
+        SoftAssert softAssert = new SoftAssert();
+
+        final String[] expectedTexts = {
+                "Payment details",
+                "We will send you a payment link via email.",
+                "Name",
+                "Email",
+                "I would like to receive order updates and promotional messages.",
+                "Submit"
+        };
+
+        PaymentDetailModal paymentModal = new MenuPage(driver)
+                .clickDrink(ESPRESSO_DRINK)
+                .clickTotalButton();
+
+        softAssert.assertEquals(paymentModal.getHeaderText(), expectedTexts[0], "Header text mismatch");
+        softAssert.assertEquals(paymentModal.getParagraphText(), expectedTexts[1], "Paragraph text mismatch");
+        softAssert.assertEquals(paymentModal.getLabelNameText(), expectedTexts[2], "Name label text mismatch");
+        softAssert.assertEquals(paymentModal.getLabelEmailText(), expectedTexts[3], "Email label text mismatch");
+        softAssert.assertEquals(paymentModal.getLabelCheckboxText(), expectedTexts[4], "Checkbox label text mismatch");
+        softAssert.assertEquals(paymentModal.getSubmitButtonText(), expectedTexts[5], "Submit button text mismatch");
+        softAssert.assertFalse(paymentModal.isCheckboxMarked(), "Checkbox should be unchecked by default");
+
+        SuccessfulPopUp successPopup = paymentModal
+                .enterName(testValueProvider.getUserName())
+                .enterEmail(testValueProvider.getUserEmail())
+                .clickSubmitButtonWithValidInput();
+
+        softAssert.assertTrue(successPopup.isDisplayed(), "Successful popup should be visible");
+        softAssert.assertEquals(successPopup.getSuccessTitleText(), SUCCESS_MESSAGE, "Success message text mismatch");
+
+        softAssert.assertAll();
+    }
+}
