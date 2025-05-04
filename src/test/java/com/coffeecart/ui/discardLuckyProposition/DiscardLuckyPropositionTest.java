ec268c643edeec34be028c144dbca61358d03e8a
Dmytro Zubenko <fokist@outlook.com>
2025-05-04 14:12:15 +0300

    test corrected using enums

--- src/test/java/com/coffeecart/ui/discardLuckyProposition/DiscardLuckyPropositionTest.java
+++ src/test/java/com/coffeecart/ui/discardLuckyProposition/DiscardLuckyPropositionTest.java
@@ -1,5 +1,6 @@
 package com.coffeecart.ui.discardLuckyProposition;
 
+import com.coffeecart.data.DrinkEnum;
 import com.coffeecart.ui.component.ShortItemComponent;
 import com.coffeecart.ui.page.MenuPage;
 import com.coffeecart.ui.testrunners.BaseTestRunner;
@@ -15,31 +16,36 @@ public class DiscardLuckyPropositionTest extends BaseTestRunner {
 
     @Test
     public void testLuckyPropositionDiscard() {
-        String flatWhite = "Flat White";
-        String americano = "Americano";
-        String espressoMacchiato = "Espresso Macchiato";
-        String espresso = "Espresso";
-        String discountedDrink = "Mocha";
+        DrinkEnum flatWhite = DrinkEnum.FLAT_WHITE;
+        DrinkEnum americano = DrinkEnum.AMERICANO;
+        DrinkEnum espressoMacchiato = DrinkEnum.ESPRESSO_MACCHIATO;
+        DrinkEnum espresso = DrinkEnum.ESPRESSO;
+        DrinkEnum discountedDrink = DrinkEnum.MOCHA;
 
         MenuPage menuPage = new MenuPage(driver);
 
-        menuPage.clickDrink(flatWhite)
-                .clickDrink(flatWhite)
-                .clickDrink(flatWhite)
+        menuPage.clickDrink(flatWhite.getRecipe().getName())
+                .clickDrink(flatWhite.getRecipe().getName())
+                .clickDrink(flatWhite.getRecipe().getName())
                 .getGetLackyDayComponent().clickSkip()
                 .getButtonElement()
                 .hoverTotalButton()
                 .getShortItems().stream()
-                .filter(item -> item.getName().equals(flatWhite))
+                .filter(item -> item.getName().equals(flatWhite.getRecipe().getName()))
                 .findFirst()
                 .ifPresent(item -> item.clickMinus().clickMinus().clickMinus());
 
-        menuPage.clickDrink(americano)
-                .clickDrink(espressoMacchiato)
-                .clickDrink(espresso)
+        menuPage.clickDrink(americano.getRecipe().getName())
+                .clickDrink(espressoMacchiato.getRecipe().getName())
+                .clickDrink(espresso.getRecipe().getName())
                 .getGetLackyDayComponent().clickSkip();
 
-        List<String> expected = new ArrayList<>(List.of(americano, espressoMacchiato, espresso));
+        List<String> expected = new ArrayList<>(List.of(
+                americano.getRecipe().getName(),
+                espressoMacchiato.getRecipe().getName(),
+                espresso.getRecipe().getName()
+        ));
+
         List<String> actual = menuPage.getButtonElement()
                 .hoverTotalButton()
                 .getShortItems().stream()
@@ -52,7 +58,7 @@ public class DiscardLuckyPropositionTest extends BaseTestRunner {
         SoftAssert softAssert = new SoftAssert();
 
         softAssert.assertEquals(actual, expected, "Cart contains unexpected drinks");
-        softAssert.assertFalse(actual.contains(discountedDrink), "Mocha should NOT be in the cart");
+        softAssert.assertFalse(actual.contains(discountedDrink.getRecipe().getName()), "Mocha should NOT be in the cart");
 
         softAssert.assertAll();
     }
